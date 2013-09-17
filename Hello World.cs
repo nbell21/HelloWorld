7b1735971cafe0376fb9876d83262e6ab2abc310
nbell21 <nathanandrewbell@gmail.com>
2013-09-16 20:35:33 -0400

    Attached Hello World Programs

---
+++ Hello World.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Class1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Hello World.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Nathan
+#
+# Created:     02/09/2013
+# Copyright:   (c) Nathan 2013
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+print("Hello World!")
